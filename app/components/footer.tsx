d9f5f4f0573027a1c503bd1ec79b0a6920532c6d
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2024-04-24 05:30:44 +0530

    property config

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
       <footer className="bg-gray-800 py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-center text-gray-400">
-            <p>&copy; 2023 Property Rental Platform. All rights reserved.</p>
+            <p>&copy; 2023 DreamRent. All rights reserved.</p>
           </div>
         </div>
       </footer>
